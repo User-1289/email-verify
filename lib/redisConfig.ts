bcf63a4628185c5e14a18fdbd07003a08409a686
Armaan Zeyad <109156434+User-1289@users.noreply.github.com>
2025-01-18 12:07:46 +0530

    Update redisConfig.ts

--- lib/redisConfig.ts
+++ lib/redisConfig.ts
@@ -5,7 +5,7 @@ export function configRedis() {
         password: process.env.REDIS_DB_PASSWORD,
         socket: {
             host: 'redis-16708.c15.us-east-1-2.ec2.redns.redis-cloud.com',
-            port: 19833
+            port: 16708
         }
     })
     return client
